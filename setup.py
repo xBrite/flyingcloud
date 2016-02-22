e249112603b2d3e8ed5165854e3d181f1e2d0bd9
Adam Feuer <adam@cookbrite.com>
2016-02-21 17:34:06 -0800

    metakube command set up via setup.py entry_point

--- setup.py
+++ setup.py
@@ -1,39 +1,46 @@
+#!/usr/bin/env python
+
 from __future__ import absolute_import
 
 from setuptools import setup, find_packages
 
 setup(
-    name='flyingcloud',
-    version='0.1.15',
-    description='Build Docker images using SaltStack',
-    author='CookBrite, Inc.',
-    author_email='flyingcloud-admin@cookbrite.com',
-    license='Apache Software License 2.0',
-    url='https://github.com/cookbrite/flyingcloud',
-    packages=find_packages(exclude='tests'),
-    install_requires=[
-        'boto',
-        'boto3',
-        'docker-py',
-        'jinja2',
-        'jmespath',
-        'requests',
-        'sh',
-        'six'
-    ],
+        name='flyingcloud',
+        version='0.1.15',
+        description='Build Docker images using SaltStack',
+        author='CookBrite, Inc.',
+        author_email='flyingcloud-admin@cookbrite.com',
+        license='Apache Software License 2.0',
+        url='https://github.com/cookbrite/flyingcloud',
+        packages=find_packages(exclude='tests'),
+        install_requires=[
+            'boto',
+            'boto3',
+            'docker-py',
+            'jinja2',
+            'jmespath',
+            'requests',
+            'sh',
+            'six'
+        ],
 
-    classifiers=['Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Topic :: Utilities',
-        'Topic :: System :: Clustering',
-        'Topic :: System :: Distributed Computing'],
-    long_description=open('README.rst').read(),
-    keywords="docker saltstack devops automation",
-    )
+        classifiers=['Programming Language :: Python',
+                     'Programming Language :: Python :: 2.7',
+                     'Development Status :: 3 - Alpha',
+                     'Environment :: Console',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Information Technology',
+                     'Intended Audience :: System Administrators',
+                     'License :: OSI Approved :: Apache Software License',
+                     'Operating System :: POSIX :: Linux',
+                     'Topic :: Utilities',
+                     'Topic :: System :: Clustering',
+                     'Topic :: System :: Distributed Computing'],
+        long_description=open('README.rst').read(),
+        keywords="docker saltstack devops automation",
+        entry_points={
+            'console_scripts': [
+                'metakube = metakube.metakube:main'
+            ],
+        }
+)
