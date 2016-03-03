6e471e3c887f42551fb6a8bfdc81c10818e3f194
George V. Reilly <george@cookbrite.com>
2016-03-02 23:18:58 -0800

    Add override for acceptance test's base url

--- examples/flask-web-service/salt/app/application/flask_example_app/tests/acceptance_test.py
+++ examples/flask-web-service/salt/app/application/flask_example_app/tests/acceptance_test.py
@@ -3,14 +3,19 @@
 # -*- coding: utf-8 -*-
 
 import os
+import urlparse
+
 import unittest
 import requests
 
 
 class AppAcceptanceTests(unittest.TestCase):
     APP_PORT = os.environ.get('APP_PORT', 80)
+    BASE_URL = os.environ.get('BASE_URL', "http://localhost:{}/".format(APP_PORT))
+
     def test_canny(self):
-        r = requests.get("http://localhost:{}/canny".format(self.APP_PORT))
+        canny_url = urlparse.urljoin(self.BASE_URL, "/canny")
+        r = requests.get(canny_url)
         r.raise_for_status()
 
 
--- examples/flask-web-service/salt/testrunner/layer.py
+++ examples/flask-web-service/salt/testrunner/layer.py
@@ -10,15 +10,7 @@ from flyingcloud import BuildLayerBase, CommandError
 class TestRunner(BuildLayerBase):
     def build(self, namespace):
         test_type = namespace.test_type
-        image_name = self.SourceImageName
-        if self.PullLayer:
-            self.docker_pull(namespace, image_name)
-
-        namespace.logger.info("Running tests: type=%s", test_type)
         test_path = "/venv/lib/python2.7/site-packages/flask_example_app/tests"
-        container_id = self.docker_create_container(
-            namespace, None, image_name)
-        self.docker_start(namespace, container_id)
 
         if test_type == "unit":
             test_file = os.path.join(test_path, "unit_test.py")
@@ -26,6 +18,20 @@ class TestRunner(BuildLayerBase):
             test_file = os.path.join(test_path, "acceptance_test.py")
         else:
             raise ValueError("Unknown test_type: {}".format(test_type))
+
+        image_name = self.SourceImageName
+        if self.PullLayer:
+            self.docker_pull(namespace, image_name)
+
+        environment = {}
+        if namespace.base_url:
+            environment['BASE_URL'] = namespace.base_url
+
+        namespace.logger.info(
+            "Running tests: type=%s, environment=%r", test_type, environment)
+        container_id = self.docker_create_container(
+            namespace, None, image_name, environment=environment)
+        self.docker_start(namespace, container_id)
 
         cmd = ["/venv/bin/python", test_file, "--verbose"]
         result, full_output = self.docker_exec(
@@ -44,3 +50,6 @@ class TestRunner(BuildLayerBase):
             '--test-type', '-T',
             default='unit',
             help="Test Type: 'unit' or 'acceptance'. Default: %(default)s")
+        subparser.add_argument(
+            '--base-url', '-B',
+            help="Base URL for Acceptance tests")
