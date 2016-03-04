7490b30ace12a5097881f9745258a58c1c4189e8
Adam Feuer and George V. Reilly <pair+adam+george@cookbrite.com>
2016-03-03 16:31:05 -0800

    Rename flask-web-service -> flask-web-app

--- examples/flask-web-service/salt/app/flask-example.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec /venv/bin/uwsgi -s /tmp/uwsgi.sock --chdir /venv/lib/python2.7/site-packages/flask_example_app --module app --callable app --uid www-data --gid www-data
--- examples/flask-web-service/salt/app/nginx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-exec /usr/sbin/nginx -c /etc/nginx/nginx.conf  -g "daemon off;"
-
