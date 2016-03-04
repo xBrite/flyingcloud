dce2d9be8eff3baf089da2630cbdb10375cf2303
Adam Feuer and George V. Reilly <pair+adam+george@cookbrite.com>
2016-03-03 16:33:17 -0800

    Reformat some config

---
+++ examples/flask-web-app/salt/app/flask-example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+exec /venv/bin/uwsgi \
+    -s /tmp/uwsgi.sock \
+    --chdir /venv/lib/python2.7/site-packages/flask_example_app \
+    --module app \
+    --callable app \
+    --uid www-data \
+    --gid www-data
