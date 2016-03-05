0192ce6368a6a80afe0e4196bbf2d67a55d6654e
Adam Feuer <adam@cookbrite.com>
2016-03-04 16:38:53 -0800

    reordered app.py methods to make them interesting

--- examples/flask-web-app/salt/app/application/flask_example_app/app.py
+++ examples/flask-web-app/salt/app/application/flask_example_app/app.py
@@ -12,30 +12,6 @@ import cv2
 from PIL import Image
 from flask import Flask, make_response, request, send_from_directory, jsonify, render_template, send_file
 
-
-def configure_logging():
-    namespace = {}
-    namespace['base_dir'] = os.path.abspath(os.path.dirname(__file__))
-    namespace['logfile'] = os.path.join(namespace['base_dir'], "flask-example.log")
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    rotating_file_handler = RotatingFileHandler(namespace['logfile'], maxBytes=10000, backupCount=20)
-    rotating_file_handler.setLevel(logging.DEBUG)
-    console_stream_handler = logging.StreamHandler()
-    console_stream_handler.setLevel(logging.DEBUG if namespace.get('debug') else logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_stream_handler.setFormatter(formatter)
-    rotating_file_handler.setFormatter(formatter)
-    logger.addHandler(console_stream_handler)
-    logger.addHandler(rotating_file_handler)
-    return logger, rotating_file_handler, console_stream_handler
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
 app = Flask(__name__, static_url_path='')
 app.config.update(
         DEBUG=True,
@@ -43,6 +19,28 @@ app.config.update(
 
 # Generate a secret random key for the session
 app.secret_key = os.urandom(24)
+
+
+@app.route('/')
+def index(name=None):
+    return render_template('index.html', name=name)
+
+
+@app.route('/canny')
+def canny():
+    # adapted from https://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_canny/py_canny.html#canny
+    img = cv2.imread('static/img/ship.jpg', 0)
+    edges = cv2.Canny(img, 100, 200)
+    im = Image.fromarray(edges)
+    img2 = StringIO()
+    im.save(img2, format="png")
+    img2.seek(0)
+    return send_file(img2, mimetype='image/png')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.errorhandler(403)
@@ -72,23 +70,6 @@ def send_web(path):
     return send_from_directory('static', path)
 
 
-@app.route('/')
-def index(name=None):
-    return render_template('index.html', name=name)
-
-
-@app.route('/canny')
-def canny():
-    # adapted from https://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_canny/py_canny.html#canny
-    img = cv2.imread('static/img/ship.jpg', 0)
-    edges = cv2.Canny(img, 100, 200)
-    im = Image.fromarray(edges)
-    img2 = StringIO()
-    im.save(img2, format="png")
-    img2.seek(0)
-    return send_file(img2, mimetype='image/png')
-
-
 @app.errorhandler(500)
 def internal_error(exception):
     app.logger.error(exception)
@@ -99,6 +80,24 @@ def internal_error(exception):
 def internal_error(exception):
     app.logger.exception("Error: %s", exception)
     return render_template('400.html'), 500
+
+
+def configure_logging():
+    namespace = {}
+    namespace['base_dir'] = os.path.abspath(os.path.dirname(__file__))
+    namespace['logfile'] = os.path.join(namespace['base_dir'], "flask-example.log")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    rotating_file_handler = RotatingFileHandler(namespace['logfile'], maxBytes=10000, backupCount=20)
+    rotating_file_handler.setLevel(logging.DEBUG)
+    console_stream_handler = logging.StreamHandler()
+    console_stream_handler.setLevel(logging.DEBUG if namespace.get('debug') else logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_stream_handler.setFormatter(formatter)
+    rotating_file_handler.setFormatter(formatter)
+    logger.addHandler(console_stream_handler)
+    logger.addHandler(rotating_file_handler)
+    return logger, rotating_file_handler, console_stream_handler
 
 
 if __name__ == '__main__':
