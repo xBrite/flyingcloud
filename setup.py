eaf5684c8f73c25cc7dc7a9f348a277ee29b8df0
George V. Reilly <george@cookbrite.com>
2016-03-05 18:41:23 -0800

    Bump to v0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flyingcloud',
-    version='0.2.0',
+    version='0.2.1',
     description='Build Docker images using SaltStack',
     author='CookBrite, Inc.',
     author_email='flyingcloud-admin@cookbrite.com',
