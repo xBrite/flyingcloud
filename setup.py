43bc24bf5db84be312db6b4fce7d533580fc3ce2
George V. Reilly <george@cookbrite.com>
2016-12-01 00:45:54 -0800

    Bump to 0.3.13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flyingcloud',
-    version='0.3.12',
+    version='0.3.13',
     description='Build Docker images using SaltStack',
     author='MetaBrite, Inc.',
     author_email='flyingcloud-admin@metabrite.com',
