0c1f67bbb91c0139c4d14b77bb80cbdd5fd3f1f1
Chris Petersen <chris@metabrite.com>
2017-06-20 15:56:37 -0700

    Python 3 compatibility: unicode, xrange

--- flyingcloud/utils/misc.py
+++ flyingcloud/utils/misc.py
@@ -1,10 +1,12 @@
 
+import six
+from builtins import range
 
 def hexdump(src, length=8):
     """Generate ASCII hexdump of bytes or unicode data."""
     result = []
-    digits = 4 if isinstance(src, unicode) else 2
-    for i in xrange(0, len(src), length):
+    digits = 4 if isinstance(six.text_type) else 2
+    for i in range(0, len(src), length):
         s = src[i:i+length]
         hexa = b' '.join(["%0*X" % (digits, ord(x))  for x in s])
         text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.'  for x in s])
