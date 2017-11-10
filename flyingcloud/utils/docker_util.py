ab3050cfb41c0e203e6cfc8b3f3ec0812280d429
George V. Reilly <george@cookbrite.com>
2017-11-09 23:21:19 -0800

    Fix retry_call for Python 3; fix logging

--- flyingcloud/utils/docker_util.py
+++ flyingcloud/utils/docker_util.py
@@ -7,14 +7,14 @@ from docker.errors import APIError, DockerException
 from .. import exceptions
 
 
-def retry_call(call, logger, retries, *args, **kwargs):
+def retry_call(call, name, logger, retries, *args, **kwargs):
     """ call a function call(), retries times, with *args and **kwargs, and with
     exponential backoff on failure.  if it fails after after retries time, raise
     the last exception """
     exc = None
-    for i in xrange(retries):
+    for i in range(retries):
         try:
-            logger.info("calling %r, attempt %d/%d", call, i+1, retries)
+            logger.info("calling %r, attempt %d/%d", name, i+1, retries)
             return call(*args, **kwargs)
 
         except (APIError, DockerException, exceptions.DockerResultError) as exc:
